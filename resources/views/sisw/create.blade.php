4e354311a3b35130dadf196ca5c7f9cd50bb54a3
Riyan afriyanda <riyan.afriyanda@students.utdi.ac.id>
2022-10-28 16:01:48 +0700

    initial

---
+++ resources/views/sisw/create.blade.php
@@ -0,0 +1,66 @@
+@extends('template')
+
+@section('content')
+<div class="row mt-5 mb-5">
+    <div class="col-lg-12 margin-tb">
+        <div class="float-left">
+            <h2>Create New Siswa</h2>
+        </div>
+        <div class="float-right">
+            <a class="btn btn-secondary" href="{{ route('sisw.index') }}"> Kembali</a>
+        </div>
+    </div>
+</div>
+
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <strong>Whoops!</strong> Input gagal.<br><br>
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+
+<form action="{{ route('sisw.store') }}" method="POST">
+    @csrf
+
+     <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>no_pendaftaran:</strong>
+                <input type="text" name="no_pendaftaran" class="form-control" placeholder="NO PEND">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>nama:</strong>
+                <input type="text" name="nama" class="form-control" placeholder="NAMA SISWA">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>bahasaindo:</strong>
+                <input type="text" name="nilai_ind" class="form-control" placeholder="Nilai bahasa">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>ipa:</strong>
+                <input type="text" name="nilai_ipa" class="form-control" placeholder="ipa">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>mtk:</strong>
+                <input type="text" name="nilai_mtk" class="form-control" placeholder="mtk">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </div>
+    </div>
+
+</form>
+@endsection
